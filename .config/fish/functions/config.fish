ef8969e6a79ddf9431c448633a66137a7e79e046
Przemysław Sulecki <psugrg@gmail.com>
2024-01-08 16:49:08 +0100

    fish configuration removed

--- .config/fish/functions/config.fish
+++
@@ -1,7 +0,0 @@
-# Function used to manage dotfiles
-#
-# Usage is the same as for git since config is a special alias for it
-#
-function config --description 'Manage dotfiles'
-  /usr/bin/git --git-dir=/home/p/.cfg/ --work-tree=/home/p $argv
-end
