8f73561543e001e7982daf16bcdc2ec2f34494fd
Przemyslaw Sulecki <psugrg@gmail.com>
2025-02-27 22:21:06 +0100

    neovim deps added

--- .cfg/bin/deps-install.sh
+++ .cfg/bin/deps-install.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 
 # Install dependencies
-sudo apt-get update && \
-  sudo apt-get install -y \
-  ranger \
-  wget \
+sudo apt-get update && sudo apt-get install -y \
   fzf \
+  npm \
+  python3 \
+  python3-pip \
+  python3-venv \
+  ranger \
+  tmux \
+  wget \
   zsh \
   zsh-autosuggestions \
   zsh-common \
-  zsh-syntax-highlighting \
-  tmux
+  zsh-syntax-highlighting
+
+# Install Deno (needed by neo-vim markdown-preview plugin)
+curl -fsSL https://deno.land/install.sh | sh
+
+# Install lazy-git
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
 
 # Install Starship prompt
 # First, create a folder required by starship
@@ -20,8 +32,8 @@ sudo curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 # Nerd Fonts to be installed
 # https://www.nerdfonts.com/font-downloads
-NERD_FONTS="SourceCodePro SpaceMono CascadiaCode CascadiaMono"
-NERD_FONTS_VER=v3.1.1
+NERD_FONTS="CascadiaMono"
+NERD_FONTS_VER=v3.3.0
 
 # Download fonts
 for FONT in $NERD_FONTS
